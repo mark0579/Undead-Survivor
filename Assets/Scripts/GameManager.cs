22630a5bac20c5fa3e76e5eaeeef248c675e5751
calmdownman <cskagud7@naver.com>
2023-02-17 18:21:19 +0900

    Step 10. Create HUD 1.0
    TimeLine - 37:07

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,11 +9,14 @@ public class GameManager : MonoBehaviour
     public float gameTime;
     public float maxGameTime = 20f;
     [Header("# Player Info")]
+    public int health;
+    public int maxHealth = 100;
     public int level;
     public int kill;
     public int exp;
+    
     [HideInInspector]
-    private int[] nextExp = { 3, 5, 10, 100, 150, 210, 280, 360, 450, 600 };
+    public int[] nextExp = { 3, 5, 10, 100, 150, 210, 280, 360, 450, 600 };
     [Header("# Game Object")]
     public Player player;
     public PoolManager poolManager;
---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public enum InfoType {Exp,Level,Kill,Time,Health } //enum 타입은 세미콜론 찍지 않음
+    public InfoType type;
+
+    Text myText;
+    Slider mySlider;
+
+    private void Awake()
+    {
+        myText = GetComponent<Text>();
+        mySlider = GetComponent<Slider>();
+    }
+
+    private void LateUpdate()
+    {
+        switch (type)
+        {
+            case InfoType.Exp:
+                float curExp = GameManager.instance.exp;
+                float maxExp = GameManager.instance.nextExp[GameManager.instance.level];
+                mySlider.value = curExp/maxExp;
+                break;
+            case InfoType.Level:
+                myText.text = string.Format("Lv.{0:F0}",GameManager.instance.level);
+                break; 
+            case InfoType.Kill:
+                myText.text = string.Format("{0:F0}", GameManager.instance.kill); //F0,1,2,3.. : 소숫점 나타내는 방식
+                break;
+            case InfoType.Time:
+                float remainTime = GameManager.instance.maxGameTime - GameManager.instance.gameTime;
+                int min = Mathf.FloorToInt(remainTime / 60);
+                int sec = Mathf.FloorToInt(remainTime % 60);
+                myText.text = string.Format("{0:D2}:{1:D2}",min,sec); //D0,1,2,3.. : 자리 수 지정
+                break;
+            case InfoType.Health:
+                break;           
+        }
+    }
+}
