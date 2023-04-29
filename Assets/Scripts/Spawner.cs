92ea23e332ac260b95c252e1d205b8f25a4bda40
calmdownman <cskagud7@naver.com>
2023-04-29 19:15:29 +0900

    123

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -36,7 +36,7 @@ public class Spawner : MonoBehaviour
     void Spawn()
     {
         GameObject enemy = GameManager.instance.poolManager.Get(0);
-        enemy.transform.position = spawnPoint[Random.RandomRange(1,spawnPoint.Length)].position;
+        enemy.transform.position = spawnPoint[Random.Range(1,spawnPoint.Length)].position;
         enemy.GetComponent<Enemy>().Init(spawnData[level]);
     }
 }
